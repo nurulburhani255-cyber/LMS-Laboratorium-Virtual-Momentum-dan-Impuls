a3f66fd5cbfa3ebe5e17abfdf8f34cc901396a03
nurulburhani255-cyber <nurulburhani255@gmail.com>
2026-09-15 08:54:47 +0700

    Create route.js

---
+++ api/login/route.js
@@ -0,0 +1,38 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export async function POST(request) {
+  const body = await request.json();
+
+  const { email, password } = body;
+
+  const user = await prisma.user.findUnique({
+    where: {
+      email: email
+    }
+  });
+
+  if (!user) {
+    return Response.json({
+      success: false,
+      message: "Email tidak ditemukan"
+    });
+  }
+
+  if (user.password !== password) {
+    return Response.json({
+      success: false,
+      message: "Password salah"
+    });
+  }
+
+  return Response.json({
+    success: true,
+    user: {
+      id: user.id,
+      nama: user.nama,
+      role: user.role
+    }
+  });
+}
